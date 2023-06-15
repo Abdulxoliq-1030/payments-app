98ded413a7c8beefc8c707cef4764396e0ee36dc
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-06-15 19:01:01 +0500

    add new codes

---
+++ server.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").load();
+}
+
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/store", function (req, res) {
+  fs.readFile("items.json", function (err, data) {
+    if (err) {
+      res.status(500).end();
+    } else {
+      res.render("store.ejs", {
+        items: JSON.parse(data),
+      });
+    }
+  });
+});
+
+app.listen(4000);
