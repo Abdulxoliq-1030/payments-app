80edda81acc34d1fb0b5abe01f2e84c763705471
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-06-15 21:30:19 +0500

    finish codes

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ if (process.env.NODE_ENV !== "production") {
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
-console.log("Hll");
-
 const express = require("express");
 const app = express();
+app.use(express.json());
 const fs = require("fs");
+const { request } = require("https");
+const stripe = require("stripe")(stripeSecretKey);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -20,8 +21,42 @@ app.get("/store", function (req, res) {
       res.status(500).end();
     } else {
       res.render("store.ejs", {
+        stripePublicKey: stripePublicKey,
         items: JSON.parse(data),
       });
+    }
+  });
+});
+
+app.post("/purchase", function (req, res) {
+  fs.readFile("items.json", function (err, data) {
+    if (err) {
+      res.status(500).end();
+    } else {
+      const itemsJson = JSON.parse(data);
+      const itemsArray = itemsJson.music.concat(itemsJson.merch);
+      let total = 0;
+      req.body.items.forEach((item) => {
+        const itemJson = itemsArray.find(function (i) {
+          return i.id == item.id;
+        });
+        total = total + itemJson.price * item.quantity;
+      });
+
+      stripe.charges
+        .create({
+          amount: total,
+          source: request.body.stripeTokenId,
+          currency: "usd",
+        })
+        .then(function () {
+          console.log("Charge Successful");
+          res.json({ message: "Successfully purchased items " });
+        })
+        .catch(function () {
+          console.log("Charge Fail");
+          res.status(500).end();
+        });
     }
   });
 });
